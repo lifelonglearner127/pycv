3a437e5e3d7ef842c0970f53c3e1ec6ac205e99f
lifelonglearner127 <life.long.learner127@outlook.com>
2020-04-20 15:26:48 -0400

    tracking; add ball movement

--- projects/object_tracking/ball_movement.py
+++ projects/object_tracking/ball_movement.py
@@ -17,9 +17,13 @@ args = vars(ap.parse_args())
 green_lower = (29, 86, 6)
 green_upper = (64, 255, 255)
 pts = deque(maxlen=args["buffer"])
+counter = 0
+(d_x, d_y) = (0, 0)
+direction = ""
 
 if not args.get("video", False):
     vs = VideoStream(src=0).start()
+# otherwise, grab a reference to the video file
 else:
     vs = cv2.VideoCapture(args["video"])
 
@@ -60,11 +64,31 @@ while True:
     for i in range(1, len(pts)):
         if pts[i - 1] is None or pts[i] is None:
             continue
+
+        if counter > 10 and len(pts) == args["buffer"] and i == 1:
+            d_x = pts[-10][0] - pts[i][0]
+            d_y = pts[-10][1] - pts[i][1]
+            (dir_x, dir_y) = ("", "")
+
+            if np.abs(d_x) > 20:
+                dir_x = "East" if np.sign(d_x) == 1 else "West"
+
+            if np.abs(d_y) > 20:
+                dir_y = "North" if np.sign(d_y) == 1 else "South"
+
+            if dir_x != "" and dir_y != "":
+                direction = f"{dir_y}-{dir_x}"
+            else:
+                direction = dir_x if dir_x != "" else dir_y
+
         thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
         cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
+    cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+                0.65, (0, 0, 255), 1)
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
+    counter += 1
     if key == ord("q"):
         break
 
